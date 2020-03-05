e9387d67ac5d09b85571185413d7923f7ac59c59
magicdboyforce <magicdboyforce@yahoo.co.jp>
2020-03-05 14:52:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -44,10 +44,9 @@ app.use('/create_token', create_token);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  //var err = new Error('Not Found');
-  //err.status = 404;
-  //next(err);
-  res.status(404).send('call');
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // will print stacktrace
