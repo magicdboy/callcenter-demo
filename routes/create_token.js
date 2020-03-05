d4d50ed34076e5b29ce6c31fdf04e738d4a80a7d
magicdboyforce <magicdboyforce@yahoo.co.jp>
2020-03-05 14:42:05 +0900

    Update create_token.js

--- routes/create_token.js
+++ routes/create_token.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var twilio = require('twilio');
 var util = require('util');
 
-const AccessToken = require('twilio').jwt.AccessToken;
+const AccessToken = require('twilio').AccessToken;
 const twilioAccountSid = 'ACa0899a5c95f91514458c1af6f33708c4';
 const twilioApiKey = 'SKcee42ff41c96fd8f9d7e26a146a0f122';
 const twilioApiSecret = 'iCQKjgfWlWXEi31H7YaCM3SB56WisXv2';
