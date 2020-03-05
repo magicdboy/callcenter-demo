522dce2378e3afaacd2400b82a6c53a59f9d4398
magicdboyforce <magicdboyforce@yahoo.co.jp>
2020-03-05 13:58:06 +0900

    Update create_token.js

--- routes/create_token.js
+++ routes/create_token.js
@@ -3,15 +3,11 @@ var router = express.Router();
 var twilio = require('twilio');
 var util = require('util');
 
-var appSid = process.env.TERRA_CALL_CENTER_SID;
-var accountSid = process.env.TWILIO_ACCOUNT_SID;
-var authToken = process.env.TWILIO_AUTH_TOKEN;
-var twillioPhoneNum = process.env.TWILIO_PHONE_NUMBER;
-
-var capability = new twilio.Capability(accountSid, authToken);
-capability.allowClientOutgoing(appSid);
-capability.allowClientIncoming("support_agent");
-var token = capability.generate(600);
+const AccessToken = require('twilio').jwt.AccessToken;
+const twilioAccountSid = 'ACa0899a5c95f91514458c1af6f33708c4';
+const twilioApiKey = 'SKcee42ff41c96fd8f9d7e26a146a0f122';
+const twilioApiSecret = 'iCQKjgfWlWXEi31H7YaCM3SB56WisXv2';
+const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret);
 
 router.post('/', function(req, res, next) {
 	res.writeHead(200, {'content-type': 'text/json' });
